aa05f1660324db3b6f93f9cc604a98b22860f2ad
Azmi <muhammadazminuriman5@gmail.com>
2026-06-07 21:06:43 +0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function goToUrl(url){
+  window.location.href = url;
+}
+
+function openEnv(){
+  const envFlap = document.getElementById('envFlap');
+  if (!envFlap) return;
+  envFlap.classList.add('open');
+  setTimeout(()=> window.location.href='page2.html',850);
+}
+
+function goToUrl(url){
+  window.location.href = url;
+}
+
+function openEnv(){
+  const envFlap = document.getElementById('envFlap');
+  if (!envFlap) return;
+  envFlap.classList.add('open');
+  setTimeout(()=> window.location.href='page2.html',850);
+}
+
+function goToPage8(){
+  window.location.href = 'page8.html';
+}
